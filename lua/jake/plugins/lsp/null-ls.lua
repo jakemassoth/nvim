6651f09072054e7666f51c20c9498bd06e7de046
Jake Massoth <jake@owlin.com>
2022-12-12 10:11:18 +0000

    add eslint code actions

--- lua/jake/plugins/lsp/null-ls.lua
+++ lua/jake/plugins/lsp/null-ls.lua
@@ -7,6 +7,7 @@ end
 -- for conciseness
 local formatting = null_ls.builtins.formatting -- to setup formatters
 local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+local code_actions = null_ls.builtins.code_actions -- to setup code actions
 
 -- to setup format on save
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -22,6 +23,12 @@ null_ls.setup({
 		formatting.yamlfmt,
 		diagnostics.actionlint,
 		diagnostics.eslint_d.with({ -- js/ts linter
+			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
+			condition = function(utils)
+				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
+			end,
+		}),
+		code_actions.eslint_d.with({ -- js/ts linter
 			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
 			condition = function(utils)
 				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
